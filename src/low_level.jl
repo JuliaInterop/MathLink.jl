e9074fbe8f9f17de67d27fc4a445606aced9a337
Samuel Markson <smarkson@alum.mit.edu>
2015-12-18 14:06:12 -0500

    set mathlink library path for linux 10+

--- src/low_level.jl
+++ src/low_level.jl
@@ -12,7 +12,14 @@ include("consts.jl")
 typealias Env Ptr{Void}
 typealias Link Ptr{Void}
 
-mlib = @osx ? "/Applications/Mathematica.app/Contents/Frameworks/mathlink.framework/mathlink" : "ml64i3"
+verout = open("/tmp/checkversion.m","w")
+write(verout,"Print[\$VersionNumber]")
+close(verout)
+version=float(chomp(readall((`math -script /tmp/checkversion.m`)))
+
+mlib = "ml64i3"
+mlib = @osx ? "/Applications/Mathematica.app/Contents/Frameworks/mathlink.framework/mathlink" : mlib
+mlib = @unix ? (version>10 ? string("/usr/local/Wolfram/Mathematica/",version,"/SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions/libML64i4") : mlib) : mlib 
 macro mlib(); mlib; end
 
 function Open(path = "math")
