02025c2c3a8271807146b9eee1f29111884b0d19
Samuel Markson <smarkson@alum.mit.edu>
2016-01-16 20:07:31 -0500

    fixed st the highest version will be selected

--- src/low_level.jl
+++ src/low_level.jl
@@ -13,12 +13,12 @@ typealias Env Ptr{Void}
 typealias Link Ptr{Void}
 
 if isdir("/usr/local/Wolfram/Mathematica")
-  version=readdir("/usr/local/Wolfram/Mathematica")[1]
+  version=maximum(float(readdir("/usr/local/Wolfram/Mathematica")))
 end
 
 mlib = "ml64i3"
 mlib = @osx ? "/Applications/Mathematica.app/Contents/Frameworks/mathlink.framework/mathlink" : mlib
-mlib = @unix ? (float(version)>10 ? string("/usr/local/Wolfram/Mathematica/",version,"/SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions/libML64i4") : mlib) : mlib 
+mlib = @unix ? (version>10 ? string("/usr/local/Wolfram/Mathematica/",version,"/SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions/libML64i4") : mlib) : mlib 
 macro mlib(); mlib; end
 
 function Open(path = "math")
