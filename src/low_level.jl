f3bc335c9ec7485c10973ebd3fbe5db0c9cdf44b
Alexander Ulanouski <alexander.ulanouski@a1u.cc>
2016-02-14 11:13:57 +0100

    set correct mathlink path on OS X

--- src/low_level.jl
+++ src/low_level.jl
@@ -18,7 +18,7 @@ end
 
 mlib = "ml64i3"
 mlib = @osx ? "/Applications/Mathematica.app/Contents/Frameworks/mathlink.framework/mathlink" : mlib
-mlib = @unix ? (version>10 ? string("/usr/local/Wolfram/Mathematica/",version,"/SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions/libML64i4") : mlib) : mlib 
+mlib = @linux ? (version>10 ? string("/usr/local/Wolfram/Mathematica/",version,"/SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions/libML64i4") : mlib) : mlib 
 macro mlib(); mlib; end
 
 function Open(path = "math")
