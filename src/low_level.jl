0b3421635ce5f971d4a74bc5e822138a4bc80a95
Simon Byrne <simonbyrne@gmail.com>
2016-10-07 13:46:19 +0100

    Use UTF8 interfaces

--- src/low_level.jl
+++ src/low_level.jl
@@ -1,28 +1,26 @@
 module ML
 
-function ptr(T)
-  @assert isbits(T)
-  Array(T, 1)
-end
-
-typealias Cstr Ptr{Cchar}
-
 include("consts.jl")
 
 typealias Env Ptr{Void}
 typealias Link Ptr{Void}
 
 
-function find_lib_ker()    
+"""
+    lib,ker = find_lib_ker()
+
+Finds the MathLink library (`lib`) and kernel executable (`ker`).
+"""
+function find_lib_ker()
     @static if is_apple()
         # TODO: query OS X metadata for non-default installations
         # https://github.com/JuliaLang/julia/issues/8733#issuecomment-167981954
-        mpath = "/Applications/Mathematica.app"        
+        mpath = "/Applications/Mathematica.app"
         if isdir(mpath)
             lib = joinpath(mpath,"Contents/Frameworks/mathlink.framework/mathlink")
             ker = joinpath(mpath,"Contents/MacOS/MathKernel")
             return lib, ker
-        end        
+        end
     end
 
     @static if is_linux()
@@ -32,7 +30,7 @@ function find_lib_ker()
 
         # alternatively, "math" or "wolfram" is often in PATH, so could use
         # echo \$InstallationDirectory | math | sed -n -e 's/Out\[1\]= //p'
-        
+
         for mpath in ["/usr/local/Wolfram/Mathematica","/opt/Wolfram/WolframEngine"]
             if isdir(mpath)
                 vers = readdir(mpath)
@@ -64,11 +62,18 @@ function find_lib_ker()
         end
     end
 
-    error("Could not find Mathematica installation")        
+    error("Could not find Mathematica installation")
 end
 
 const mlib,mker = find_lib_ker()
 
+"""
+    link = Open([path])
+
+Opens the connection to the library, using kernel at `path`.
+
+See [`MLOpenString`](https://reference.wolfram.com/language/ref/c/MLOpenString.html).
+"""
 function Open(path = mker)
   # MLInitialize
   mlenv = ccall((:MLInitialize, mlib), Env, (Cstr,), C_NULL)
@@ -76,12 +81,12 @@ function Open(path = mker)
 
   # MLOpenString
   # local link
-  err = ptr(Cint)
+  err = Ref{Cint}()
   args = "-linkname '\"$path\" -mathlink' -linkmode launch"
   link = ccall((:MLOpenString, mlib), Link,
-                (Env, Cstr, Ptr{Cint}),
+                (Env, Cstr, Ref{Cint}),
                 mlenv, args, err)
-  err[1]==0 || mlerror(link, "MLOpenString")
+  err[]==0 || mlerror(link, "MLOpenString")
 
   # Ignore first input packet
   @assert NextPacket(link) == Pkt.INPUTNAME
@@ -90,83 +95,111 @@ function Open(path = mker)
   return link
 end
 
+"""
+    Close(link)
+
+Closes a MathLink connection.
+
+See [`MLClose`](https://reference.wolfram.com/language/ref/c/MLClose.html)
+"""
 Close(link::Link) = ccall((:MLClose, mlib), Void, (Link,), link)
 
 ErrorMessage(link::Link) =
-  ccall((:MLErrorMessage, mlib), Cstr, (Link,), link) |> unsafe_string
+    ccall((:MLErrorMessage, mlib), Cstr, (Link,), link) |> unsafe_string
 
 for f in [:Error :ClearError :EndPacket :NextPacket :NewPacket]
-  fstr = string("ML", f)
-  @eval $f(link::Link) = ccall(($fstr, mlib), Cint, (Link,), link)
+    fstr = string("ML", f)
+    @eval $f(link::Link) = ccall(($fstr, mlib), Cint, (Link,), link)
 end
 
-mlerror(link, name) = error("MathLink Error $(Error(link)) in $name: " * ErrorMessage(link))
+mlerror(link, name) = error("MathLink Error $(Error(link)) in $name: $(ErrorMessage(link))")
 
 # Put fns
-
-PutFunction(link::Link, name::AbstractString, nargs::Int) =
-  ccall((:MLPutFunction, mlib), Cint, (Link, Cstr, Cint),
-    link, name, nargs) != 0 || mlerror(link, "MLPutFunction")
-
-for (f, Tj, Tc) in [(:PutInteger64, Int64, Int64)
-                    (:PutInteger32, Int32, Int32)
-                    (:PutString, AbstractString, Cstr)
-                    (:PutSymbol, Symbol, Cstr)
-                    (:PutReal32, Float32, Float32)
-                    (:PutReal64, Float64, Float64)]
-  fstr = string("ML", f)
-  @eval $f(link::Link, x::$Tj) =
-          ccall(($fstr, mlib), Cint, (Link, $Tc), link, x) != 0 ||
+for (f, T) in [(:PutInteger64, Int64)
+               (:PutInteger32, Int32)
+               (:PutReal32, Float32)
+               (:PutReal64, Float64)]
+    fstr = string("ML", f)
+    @eval $f(link::Link, x::$Tj) =
+        ccall(($fstr, mlib), Cint, (Link, $Tc), link, x) != 0 ||
             mlerror(link, $fstr)
+end
+
+function PutString(link::Link, x::AbstractString)
+    s = String(x)
+    ccall((:MLPutUTF8String, mlib), Cint, (Link, Ptr{Cuchar}, Cint), link, s, length(s.data)) != 0 ||
+        mlerror(link, "MLPutUTF8String")
+end
+function PutSymbol(link::Link, x::Symbol)
+    s = string(x)
+    ccall((:MLPutUTF8Symbol, mlib), Cint, (Link, Ptr{Cuchar}, Cint), link, s, length(s.data)) != 0 ||
+        mlerror(link, "MLPutUTF8Symbol")
+end
+
+function PutFunction(link::Link, name::AbstractString, nargs)
+    s = String(name)
+    ccall((:MLPutUTF8Function, mlib), Cint, (Link, Ptr{Cuchar}, Cint, Cint), link, s, length(s.data), nargs) != 0 ||
+        mlerror(link, "MLPutUTF8Function")
 end
 
 # Get fns
 
+"""
+    GetType(link)
+
+Gets the type of the current object on `link`.
+
+See [MLGetType](http://reference.wolfram.com/language/ref/c/MLGetType.html)
+"""
 GetType(link::Link) =
-  ccall((:MLGetType, mlib), Cint, (Link,), link) |> Char
+    ccall((:MLGetType, mlib), Cint, (Link,), link) |> Char
 
 for (f, T) in [(:GetInteger64, Int64)
                (:GetInteger32, Int32)
                (:GetReal32, Float32)
                (:GetReal64, Float64)]
-  fstr = string("ML", f)
-  @eval function $f(link::Link)
-    i = ptr($T)
-    ccall(($fstr, mlib), Cint, (Link, Ptr{$T}), link, i) != 0 ||
-      mlerror(link, $fstr)
-    i[1]
-  end
+    fstr = string("ML", f)
+    @eval function $f(link::Link)
+        i = Ref{$T}()
+        ccall(($fstr, mlib), Cint, (Link, Ref{$T}), link, i) != 0 ||
+        mlerror(link, $fstr)
+        i[]
+    end
 end
 
+# http://reference.wolfram.com/language/ref/c/MLGetUTF8String.html
+# http://reference.wolfram.com/language/ref/c/MLReleaseUTF8String.html
 function GetString(link::Link)
-  s = ptr(Cstr)
-  ccall((:MLGetString, mlib), Cint, (Link, Ptr{Cstr}), link, s) != 0 ||
-    mlerror(link, "GetString")
-  r = s[1] |> unsafe_string |> unescape_string
-  ReleaseString(link, s)
-  return r
+    s = Ref{Ptr{Cuchar}}()
+    b = Ref{Cint}()
+    c = Ref{Cint}()
+    ccall((:MLGetUTF8String, mlib), Cint, (Link, Ref{Ptr{Cuchar}}, Ref{Cint}, Ref{Cint}), link, s, b, c) != 0 ||
+        mlerror(link, "MLGetUTF8String")
+    r = unsafe_string(s[], b[]) |> unescape_string
+    ccall((:MLReleaseUTF8String, mlib), Void, (Link, Ptr{Cuchar}, Cint), link, s[], b[])
+    return r
 end
 
 function GetSymbol(link::Link)
-  s = ptr(Cstr)
-  ccall((:MLGetSymbol, mlib), Cint, (Link, Ptr{Cstr}), link, s) != 0 ||
-    mlerror(link, "GetSymbol")
-  r = s[1] |> unsafe_string |> unescape_string |> Symbol
-  ReleaseSymbol(link, s)
-  return r
+    s = Ref{Ptr{Cuchar}}()
+    b = Ref{Cint}()
+    c = Ref{Cint}()
+    ccall((:MLGetUTF8Symbol, mlib), Cint, (Link, Ref{Ptr{Cuchar}}, Ref{Cint}, Ref{Cint}), link, s, b, c) != 0 ||
+        mlerror(link, "MLGetUTF8Symbol")
+    r = unsafe_string(s[], b[]) |> unescape_string |> Symbol
+    ccall((:MLReleaseUTF8Symbol, mlib), Void, (Link, Ptr{Cuchar}, Cint), link, s[], b[])
+    return r
 end
 
 function GetFunction(link::Link)
-  name = ptr(Cstr)
-  nargs = ptr(Cint)
-  ccall((:MLGetFunction, mlib), Cint, (Link, Ptr{Cstr}, Ptr{Cint}),
-    link, name, nargs) != 0 || mlerror(link, "MLGetFunction")
-  r = name[1] |> unsafe_string |> Symbol, nargs[1]
-  ReleaseString(link, name)
-  return r
+    s = Ref{Ptr{Cuchar}}()
+    b = Ref{Cint}()
+    n = Ref{Cint}()
+    ccall((:MLGetUTF8Function, mlib), Cint, Cint, (Link, Ref{Ptr{Cuchar}}, Ref{Cint}, Ref{Cint}), link, s, b, c) != 0 ||
+        mlerror(link, "MLGetUTF8Function")
+    r = unsafe_string(s[], b[]) |> unescape_string |> Symbol, n[]
+    ccall((:MLReleaseUTF8Symbol, mlib), Void, (Link, Ptr{Cuchar}, Cint), link, s[], b[])
+    return r
 end
-
-ReleaseString(link::Link, s) = ccall((:MLReleaseString, mlib), Void, (Link, Cstr), link, s[1])
-ReleaseSymbol(link::Link, s) = ccall((:MLReleaseSymbol, mlib), Void, (Link, Cstr), link, s[1])
 
 end
