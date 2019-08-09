e81be9ae38c7c16e833569298818391c30dcfe59
Simon Byrne <simonbyrne@gmail.com>
2019-08-09 14:23:39 -0700

    move errors to enums

--- src/consts.jl
+++ src/consts.jl
@@ -1,38 +1,38 @@
 # Constants - from mathlink.h
 
 @enum Packet begin
-  PKT_ILLEGAL    =   0
+    PKT_ILLEGAL    =   0
 
-  PKT_CALL       =   7
-  PKT_EVALUATE   =  13
-  PKT_RETURN     =   3
+    PKT_CALL       =   7
+    PKT_EVALUATE   =  13
+    PKT_RETURN     =   3
 
-  PKT_INPUTNAME  =   8
-  PKT_ENTERTEXT  =  14
-  PKT_ENTEREXPR  =  15
-  PKT_OUTPUTNAME =   9
-  PKT_RETURNTEXT =   4
-  PKT_RETURNEXPR =  16
+    PKT_INPUTNAME  =   8
+    PKT_ENTERTEXT  =  14
+    PKT_ENTEREXPR  =  15
+    PKT_OUTPUTNAME =   9
+    PKT_RETURNTEXT =   4
+    PKT_RETURNEXPR =  16
 
-  PKT_DISPLAY    =  11
-  PKT_DISPLAYEND =  12
+    PKT_DISPLAY    =  11
+    PKT_DISPLAYEND =  12
 
-  PKT_MESSAGE    =   5
-  PKT_TEXT       =   2
+    PKT_MESSAGE    =   5
+    PKT_TEXT       =   2
 
-  PKT_INPUT      =   1
-  PKT_INPUTSTR   =  21
-  PKT_MENU       =   6
-  PKT_SYNTAX     =  10
+    PKT_INPUT      =   1
+    PKT_INPUTSTR   =  21
+    PKT_MENU       =   6
+    PKT_SYNTAX     =  10
 
-  PKT_SUSPEND    =  17
-  PKT_RESUME     =  18
+    PKT_SUSPEND    =  17
+    PKT_RESUME     =  18
 
-  PKT_BEGINDLG   =  19
-  PKT_ENDDLG     =  20
+    PKT_BEGINDLG   =  19
+    PKT_ENDDLG     =  20
 
-  PKT_FIRSTUSER  = 128
-  PKT_LASTUSER   = 255
+    PKT_FIRSTUSER  = 128
+    PKT_LASTUSER   = 255
 end
 
 @enum Token begin
@@ -86,76 +86,75 @@ end
 
     TK_FLOAT32_LE  = 244
     TK_FLOAT64_LE  = 246
-    TK_FLOAT128_LE = 248
-    
+    TK_FLOAT128_LE = 248    
 end
 
-module ERR
-  const UNKNOWN         =   -1
-  const OK              =    0
-  const DEAD            =    1
-  const GBAD            =    2
-  const GSEQ            =    3
-  const PBTK            =    4
-  const PSEQ            =    5
-  const PBIG            =    6
-  const OVFL            =    7
-  const MEM             =    8
-  const ACCEPT          =    9
-  const CONNECT         =   10
-  const CLOSED          =   11
-  const DEPTH           =   12  # /* internal error */
-  const NODUPFCN        =   13  # /* stream cannot be duplicated */
+@enum Error begin
+    ERR_UNKNOWN         =   -1
+    ERR_OK              =    0
+    ERR_DEAD            =    1
+    ERR_GBAD            =    2
+    ERR_GSEQ            =    3
+    ERR_PBTK            =    4
+    ERR_PSEQ            =    5
+    ERR_PBIG            =    6
+    ERR_OVFL            =    7
+    ERR_MEM             =    8
+    ERR_ACCEPT          =    9
+    ERR_CONNECT         =   10
+    ERR_CLOSED          =   11
+    ERR_DEPTH           =   12  # /* internal error */
+    ERR_NODUPFCN        =   13  # /* stream cannot be duplicated */
 
-  const NOACK           =   15  # /* */
-  const NODATA          =   16  # /* */
-  const NOTDELIVERED    =   17  # /* */
-  const NOMSG           =   18  # /* */
-  const FAILED          =   19  # /* */
+    ERR_NOACK           =   15  # /* */
+    ERR_NODATA          =   16  # /* */
+    ERR_NOTDELIVERED    =   17  # /* */
+    ERR_NOMSG           =   18  # /* */
+    ERR_FAILED          =   19  # /* */
 
-  const GETENDEXPR      =   20
-  const PUTENDPACKET    =   21 # /* unexpected call of MLEndPacket */
-                               # /* currently atoms aren't
-                               # * counted on the way out so this error is raised only when
-                               # * MLEndPacket is called in the midst of an atom
-                               # */
-  const NEXTPACKET      =   22
-  const UNKNOWNPACKET   =   23
-  const GETENDPACKET    =   24
-  const ABORT           =   25
-  const MORE            =   26 # /* internal error */
-  const NEWLIB          =   27
-  const OLDLIB          =   28
-  const BADPARAM        =   29
-  const NOTIMPLEMENTED  =   30
+    ERR_GETENDEXPR      =   20
+    ERR_PUTENDPACKET    =   21 # /* unexpected call of MLEndPacket */
+    # /* currently atoms aren't
+    # * counted on the way out so this error is raised only when
+    # * MLEndPacket is called in the midst of an atom
+    # */
+    ERR_NEXTPACKET      =   22
+    ERR_UNKNOWNPACKET   =   23
+    ERR_GETENDPACKET    =   24
+    ERR_ABORT           =   25
+    ERR_MORE            =   26 # /* internal error */
+    ERR_NEWLIB          =   27
+    ERR_OLDLIB          =   28
+    ERR_BADPARAM        =   29
+    ERR_NOTIMPLEMENTED  =   30
 
 
-  const INIT            =   32
-  const ARGV            =   33
-  const PROTOCOL        =   34
-  const MODE            =   35
-  const LAUNCH          =   36
-  const LAUNCHAGAIN     =   37
-  const LAUNCHSPACE     =   38
-  const NOPARENT        =   39
-  const NAMETAKEN       =   40
-  const NOLISTEN        =   41
-  const BADNAME         =   42
-  const BADHOST         =   43
-  const RESOURCE        =   44  # /* a required resource was missing */
-  const LAUNCHFAILED    =   45
-  const LAUNCHNAME      =   46
+    ERR_INIT            =   32
+    ERR_ARGV            =   33
+    ERR_PROTOCOL        =   34
+    ERR_MODE            =   35
+    ERR_LAUNCH          =   36
+    ERR_LAUNCHAGAIN     =   37
+    ERR_LAUNCHSPACE     =   38
+    ERR_NOPARENT        =   39
+    ERR_NAMETAKEN       =   40
+    ERR_NOLISTEN        =   41
+    ERR_BADNAME         =   42
+    ERR_BADHOST         =   43
+    ERR_RESOURCE        =   44  # /* a required resource was missing */
+    ERR_LAUNCHFAILED    =   45
+    ERR_LAUNCHNAME      =   46
 
-  const PDATABAD        =   47
-  const PSCONVERT       =   48
-  const GSCONVERT       =   49
-  const NOTEXE          =   50
-  const SYNCOBJECTMAKE  =   51
-  const BACKOUT         =   52
+    ERR_PDATABAD        =   47
+    ERR_PSCONVERT       =   48
+    ERR_GSCONVERT       =   49
+    ERR_NOTEXE          =   50
+    ERR_SYNCOBJECTMAKE  =   51
+    ERR_BACKOUT         =   52
 
-  const TRACEON         =  996  # /* */
-  const TRACEOFF        =  997  # /* */
-  const DEBUG           =  998  # /* */
-  const ASSERT          =  999  # /* an internal assertion failed */
-  const USER            = 1000  # /* start of user defined errors */
+    ERR_TRACEON         =  996  # /* */
+    ERR_TRACEOFF        =  997  # /* */
+    ERR_DEBUG           =  998  # /* */
+    ERR_ASSERT          =  999  # /* an internal assertion failed */
+    ERR_USER            = 1000  # /* start of user defined errors */
 end
