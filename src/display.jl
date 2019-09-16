cd771771b8050e2520dc031f9be1e3303589ef5c
Simon Byrne <simonbyrne@gmail.com>
2019-09-16 09:34:45 -0700

    remove old files

--- src/display.jl
+++
@@ -1,7 +0,0 @@
-import Base: show
-
-show(io::IO, ::MIME"text/html", p::MExpr{:Graphics}) =
-  print(io, ExportString(p, "SVG"))
-
-show(io::IO, ::MIME"image/png", p::MExpr{:Graphics3D}) =
-  print(io, @math ExportString($p, "PNG"))
