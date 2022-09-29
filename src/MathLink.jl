34816993cf559b6637e2b72c0bd23247de772ed6
Mikael Fremling <micke.fremling@gmail.com>
2022-09-29 10:25:47 +0200

    Added a better error messade when the MathLinks instalation fails and there is no deps/deps.jl file.

--- src/MathLink.jl
+++ src/MathLink.jl
@@ -4,7 +4,17 @@ using Printf
 
 export @W_str, @W_cmd, weval
 
-include("../deps/deps.jl")
+
+###Checking that the deps.jl file exists. And throwing a readable error if it does not!
+if isfile("../deps/deps.jl")
+    include("../deps/deps.jl")
+else
+    FilePath=@__DIR__
+    FilePath=FilePath[1:end-4]*"/deps/deps.jl"
+    error("The file $FilePath does not exist.\n"
+          *"This usually means that the MathLink installation failed.\n"
+          *"Have you checked that Mathematica is installed on your system?")
+end
 include("types.jl")
 include("consts.jl")
 include("init.jl")
